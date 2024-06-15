baaf7919ffc156442242f3ca7b2a41f8c2e6d026
Dustin Stringer <dustin@negativezeroinc.com>
2024-06-15 08:59:37 -0700

    chore: Update server.js to enable CORS and add API route for GitHub integration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,20 @@
 import { } from 'dotenv/config';
 
-import fs from 'fs';
+import cors from 'cors';
+import express from 'express';
 import path from 'path';
-import simpleGit from 'simple-git';
+import githubRoutes from './routes/github.js';
 
-// const app = express();
-// const port = 3000;
+const app = express();
+const port = 3000;
 
-// app.use(cors());
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-// app.use('/api/github', githubRoutes);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.use('/api/github', githubRoutes);
 
 const gitRepoURL = process.env.GITHUB_REPO_URL;
 const gitBaseDir = process.env.LOCAL_BASE_DIR;
@@ -24,17 +25,17 @@ const workingBranchName = 'bug-fix';
 const mainBranchDir = path.join(repoDir, mainBranchName);
 const workingBranchDir = path.join(repoDir, workingBranchName);
 
-// Create the directory if it doesn't exist
-if (!fs.existsSync(workingBranchDir)) {
-  fs.mkdirSync(workingBranchDir, { recursive: true });
-}
+// // Create the directory if it doesn't exist
+// if (!fs.existsSync(workingBranchDir)) {
+//   fs.mkdirSync(workingBranchDir, { recursive: true });
+// }
 
-const git = simpleGit({ baseDir: workingBranchDir });
-await simpleGit().clone(gitRepoURL, workingBranchDir);
-await git.checkoutBranch(workingBranchName, mainBranchName);
+// const git = simpleGit({ baseDir: workingBranchDir });
+// await simpleGit().clone(gitRepoURL, workingBranchDir);
+// await git.checkoutBranch(workingBranchName, mainBranchName);
 
-await git.pull('origin', mainBranchName);
+// await git.pull('origin', mainBranchName);
 
-// app.listen(port, () => {
-//   console.log(`Server running on http://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
