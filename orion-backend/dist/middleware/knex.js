5bf9d04fad21be7ba68072c21e1a357657fdd0d2
Dustin Stringer <dustin@negativezeroinc.com>
2025-01-25 20:04:02 -0700

    remove dist

--- orion-backend/dist/middleware/knex.js
+++
@@ -1,4 +0,0 @@
-import knex from "knex";
-import config from "../config/knexfile";
-const environment = process.env.NODE_ENV || "development";
-export default knex(config[environment]);
--- orion-backend/dist/openai.js
+++
@@ -1,9 +0,0 @@
-import OpenAI from 'openai';
-const apiKey = process.env.OPENAI_API_KEY;
-if (!apiKey) {
-    throw new Error('OPENAI_API_KEY is not defined');
-}
-const openai = new OpenAI({
-    apiKey, // This ensures apiKey is always a string
-});
-export default openai;
